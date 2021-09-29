bbc9beb35d6c1c2fa297fcd026206c4cd7a4a35a
Rohitkumar20011806 <91238263+Rohitkumar20011806@users.noreply.github.com>
2021-09-29 14:09:03 +0530

    Add files via upload

---
+++ Practical 4/Task 1/task1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+namespace first
+{
+	int sum(int a,int b)
+	{
+		return(a+b);
+	}
+}
+
+namespace second
+{
+	float sum(float a,float b)
+	{
+		return(a+b);
+	}
+}
+int main()
+{
+	
+	cout<<"Sum of 2 and 3 is "<<first::sum(2,3)<<"\n";
+	cout<<"Sum is 2.1 and 3.2 is "<<second::sum(2.1,3.2)<<"\n";
+	cout<<"Sum of 2 and 3.2 is "<<second::sum(2,3.2)<<"\n";
+	cout<<"Sum of 2.1 and 3 is "<<second::sum(2.1,3)<<"\n";
+
+}
+
