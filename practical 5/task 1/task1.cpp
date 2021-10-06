ce47aaf683e33151db8a4adf0dfb635d0577ad51
Rohitkumar20011806 <91238263+Rohitkumar20011806@users.noreply.github.com>
2021-10-06 10:07:45 +0530

    Add files via upload

---
+++ practical 5/task 1/task1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+cout<<"size of  different primary datatype in c++"<<endl;
+
+cout<<"char:"<<sizeof(char)<<endl;
+ cout<<"short:"<<sizeof(short)<<endl;
+ cout<<"int:"<<sizeof(int)<<endl;
+ cout<<"long int:"<<sizeof(long int)<<endl;
+ cout<<"float:"<<sizeof(float)<<endl;
+ cout<<"double"<<sizeof(double)<<endl;
+ cout<<"wide character:"<<sizeof(wchar_t)<<endl;
+ return 0;
+}
